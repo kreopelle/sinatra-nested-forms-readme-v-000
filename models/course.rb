4d3a6857f3d62e2aaa4f4c2413ed970059befc57
Kayla Reopelle <kreopelle@gmail.com>
2018-07-13 19:12:28 +0000

    Done.

---
+++ models/course.rb
@@ -0,0 +1,15 @@
+class Course
+  attr_accessor :name, :topic
+  COURSES = []
+
+  def initialize(args)
+    @name = args[:name]
+    @topic = args[:topic]
+    COURSES << self
+  end
+
+  def self.all
+    COURSES
+  end 
+
+end
